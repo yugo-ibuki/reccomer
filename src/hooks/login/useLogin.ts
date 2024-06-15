66db1816ec68f401cc39a4005d352ded07b7c821
yugo-ibuki <y.ibuki91@gmail.com>
2024-06-15 23:53:25 +0900

    feat: Add loading state to useLogin hook

--- src/hooks/login/useLogin.ts
+++ src/hooks/login/useLogin.ts
@@ -4,10 +4,11 @@ import { useRouter } from 'next/navigation'
 import { auth } from '@/configs'
 import { useToast } from '@/hooks'
 import { useAuth } from '@/providers'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 export const useLogin = () => {
   const { user, loading, setAuthUser } = useAuth()
+  const [loginPageLoading, setLoginPageLoading] = useState<boolean>(true)
   const { toast } = useToast()
   const [error, handleError] = useErrorHandling()
   const router = useRouter()
@@ -28,14 +29,16 @@ export const useLogin = () => {
   }
 
   useEffect(() => {
+    setLoginPageLoading(true)
     if (user) {
       router.push('/dashboard')
     }
+    setLoginPageLoading(false)
   }, [router.push, user])
 
   return {
     error,
-    loading,
+    loading: loading || loginPageLoading,
     handleGoogleLogin,
   }
 }
